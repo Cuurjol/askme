23ddc210d1a4df85af7721d2e787cdf9ce16ea5e
cuurjol <cuurjol@gmail.com>
2019-01-03 17:16:04 +0300

    Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
-  has_many :questions, dependent: :nullify
+  has_many :questions, dependent: :delete_all
+  has_many :asked_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[0-9a-z_]+\z/i }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,}\z/i}
